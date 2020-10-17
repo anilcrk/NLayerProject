c425aa51189a0acfff53ce29a10b8c0db9283ef5
Anil <anilcirik37@gmail.com>
2020-10-18 02:28:24 +0300

    Add project files.

---
+++ NLayerProject.Core/Model/Category.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace NLayerProject.Core.Model
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>(); //ilk oluşturuldupunda boi bir tane collenction nesnesi oluşturuyor 
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ NLayerProject.Core/Model/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLayerProject.Core.Model
+{
+   public class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public int Stock{ get; set; }
+        public decimal Price { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public string InnerBarcode { get; set; }
+
+        public virtual Category Category { get; set; } //Category tablosuna referans veriyoruz çünkü ef kategori üzerinde izleme yapabilsin ve kayıt altına akınabilsin
+    }
+}
